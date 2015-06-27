84a2594cd63fdc7396f286fcefe6402b79c29844
Tmp Ballerandco <tmp@ballerandco.com>
2015-06-27 23:29:37 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync(infile)))
+  response.send(fs.readFileSync(infile,"utf-8"))
 })
 
 app.listen(app.get('port'), function() {
